b621e4ce3d89e4ea6fec080281797eb0fbf1b1d4
Gabriel <gabrieldvr@outlook.com>
2023-12-12 08:32:27 -0300

    fix(pusher): Update pusher.ts

--- src/lib/pusher.ts
+++ src/lib/pusher.ts
@@ -2,15 +2,15 @@ import PusherServer from "pusher";
 import PusherClient from "pusher-js";
 
 export const pusherServer = new PusherServer({
-  appId: '1718684',
-  key: '7e4d8b27417cda395ba5',
-  secret: 'c141b2f73e00d710e406',
+  appId: '1723535',
+  key: '97b95e67f3886a18c6a4',
+  secret: 'bc98d45efe3c115ec604',
   cluster: "sa1"!,
   useTLS: true,
 });
 
 export const pusherClient = new PusherClient(
-  process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
+  97b95e67f3886a18c6a4,
   {
     cluster: "sa1"!,
   }
