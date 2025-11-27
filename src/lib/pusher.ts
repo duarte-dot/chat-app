be559e9976c8f0c1a5424ef147958306886516c9
duarte <gabrieldvr@outlook.com>
2025-11-27 16:39:46 -0300

    dwdjwmd

--- src/lib/pusher.ts
+++ src/lib/pusher.ts
@@ -1,16 +1,16 @@
 import PusherServer from "pusher";
 import PusherClient from "pusher-js";
 
-const { PUSHER_APP_ID: appId, PUSHER_APP_SECRET: key, PUSHER_APP_KEY: secret, PUSHER_APP_CLUSTER: cluster } = process.env;
+const { PUSHER_APP_ID: appId, PUSHER_APP_SECRET: secret, PUSHER_APP_CLUSTER: cluster } = process.env;
 
 export const pusherServer = new PusherServer({
   appId: appId!,
-  key: key!,
+  key: "56236ec0fa30b0d11a08",
   secret: secret!,
-  cluster: cluster!,
+  cluster: "sa1",
   useTLS: true,
 });
 
-export const pusherClient = new PusherClient(key!, {
-  cluster: cluster!,
+export const pusherClient = new PusherClient("56236ec0fa30b0d11a08", {
+  cluster: "sa1",
 });
